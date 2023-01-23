92db71eb42a140e82ea64fd690b911c763f1cfab
Faith Moraa <moraaf770@gmail.com>
2023-01-23 05:36:09 +0300

    display fetched data using cards

--- src/components/botcollection.js
+++ src/components/botcollection.js
@@ -1,9 +1,18 @@
 import React from "react"
+import BotCard from "./botcard"
 
-function BotCollection(){
+function BotCollection(botCollection, userBots, setUserBots){
+
+    //using cards to display the fetched bots
+    const displayBotCards = botCollection.map(bot => {
+        return <BotCard key={bot.id} bot={bot} userBots={userBots} setUserBots={setUserBots}/>
+      })
     return(
-        <div>
-            
+        <div className="ui four column grid">
+            <div className="row">
+                {/*display the bot cards*/}
+                {displayBotCards}
+            </div>
         </div>
     )
 }
